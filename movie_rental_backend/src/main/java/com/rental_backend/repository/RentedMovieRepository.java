3a9d6fe8d0b3c9a218ca083de08e8a93c5cfba81
elifcen <elif.cenesiz@ug.bilkent.edu.tr>
2022-04-26 15:48:19 +0300

    movie repo

--- movie_rental_backend/src/main/java/com/rental_backend/repository/RentedMovieRepository.java
+++ movie_rental_backend/src/main/java/com/rental_backend/repository/RentedMovieRepository.java
@@ -1,8 +1,22 @@
 package com.rental_backend.repository;
 import com.rental_backend.entity.RentedMovie;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
 
 @Repository
 public interface RentedMovieRepository extends CrudRepository<RentedMovie,Long> {
+
+    Date now = Date.valueOf(LocalDate.now());
+
+    List<RentedMovie> findAll();
+
+    @Query
+    List<RentedMovie> getCurrentlyRented(@Param("userId") String userId);
 }
