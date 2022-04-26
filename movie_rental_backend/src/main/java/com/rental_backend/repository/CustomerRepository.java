9083c1e02cfe95f55f363f0195132db9628d0a63
elifcen <elif.cenesiz@ug.bilkent.edu.tr>
2022-04-26 15:20:35 +0300

    movie repo

--- movie_rental_backend/src/main/java/com/rental_backend/repository/CustomerRepository.java
+++ movie_rental_backend/src/main/java/com/rental_backend/repository/CustomerRepository.java
@@ -1,4 +1,18 @@
 package com.rental_backend.repository;
 
-public interface CustomerRepository {
+import com.rental_backend.entity.Customer;
+import com.rental_backend.entity.MovieRequest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+
+
+    boolean existsByEmail(String email);
+    List<Customer> findAll();
+
+
 }
--- movie_rental_backend/src/main/java/com/rental_backend/repository/MovieRepository.java
+++ movie_rental_backend/src/main/java/com/rental_backend/repository/MovieRepository.java
@@ -2,12 +2,23 @@ package com.rental_backend.repository;
 
 
 import com.rental_backend.entity.Movie;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends CrudRepository<Movie,Long>{
 
+
+    List<Movie> findAll();
+
+    @Query("select m from Movie m " +
+            "where lower(m.title) like lower(concat('%', :searchTerm, '%')) " +
+            "or lower(m.director_name) like lower(concat('%', :searchTerm, '%'))")
+    List<Movie> searchByName(@Param("searchTerm") String searchTerm);
 
 
 }
