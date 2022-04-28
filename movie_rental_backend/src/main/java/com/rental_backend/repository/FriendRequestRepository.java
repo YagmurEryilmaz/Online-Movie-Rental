3106505e59e55d18610e7351dec299496dc8c057
YagmurEryilmaz <yagmur.eryilmaz@ug.bilkent.edu.tr>
2022-04-28 16:38:25 +0300

    minor changes in FriendRequestRepository

--- movie_rental_backend/src/main/java/com/rental_backend/repository/FriendRequestRepository.java
+++ movie_rental_backend/src/main/java/com/rental_backend/repository/FriendRequestRepository.java
@@ -3,6 +3,7 @@ package com.rental_backend.repository;
 import com.rental_backend.entity.Customer;
 import com.rental_backend.entity.FriendRequest;
 import com.rental_backend.entity.Suggestion;
+import org.hibernate.annotations.SQLInsert;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,15 +14,21 @@ import java.util.List;
 @Repository
 public interface FriendRequestRepository extends CrudRepository<FriendRequest,Long> {
     @Query("select fr.receiver.name from FriendRequest fr, Customer c where fr.sender.uId = c.uId and fr.sender.uId = :senderId ")
-    List<FriendRequest> findbySenderId(@Param("senderId") String senderId);
+    List<FriendRequest> findbySenderId(@Param("senderId") Long senderId);
 
     @Query("select fr.sender.name from FriendRequest fr, Customer c where fr.receiver.uId = c.uId and fr.receiver.uId = :receiverId ")
-    List<FriendRequest> findbyReceiverId(@Param("receiverId") String receiverId);
+    List<FriendRequest> findbyReceiverId(@Param("receiverId") Long receiverId);
 
-    @Query("select count(fr.receiver.receivedRequests) from FriendRequest fr, Customer c where fr.friendReq_status = 'accepted' and fr.receiver.uId = c.uId and fr.receiver.uId = :receiverId")
-    List<FriendRequest> findNumOfFriends(@Param("receiverId") String receiverId);
+    @Query("select count(fr.receiver.receivedRequests) from FriendRequest fr, Customer c where (fr.friendReq_status = 'accepted' and fr.receiver.uId = c.uId and fr.receiver.uId = :receiverId) or (fr.friendReq_status = 'accepted' and fr.sender.uId = :senderId and fr.sender.uId = c.uId ) ")
+    List<FriendRequest> findNumOfFriends(@Param("receiverId")  Long receiverId, @Param("senderId")Long senderId);
 
-    @Query("select count(fr.receiver.receivedRequests) from FriendRequest fr, Customer c where fr.friendReq_status = 'waiting' and fr.receiver.uId = :receiverId")
-    List<FriendRequest> findNumOfWaitingRequests(@Param("receiverId") String receiverId);
+    @Query("select count(fr.receiver.receivedRequests) from FriendRequest fr, Customer c where fr.friendReq_status = 'waiting' and fr.receiver.uId = :receiverId ")
+    List<FriendRequest> findNumOfPendingRequests(@Param("receiverId") Long receiverId);
+
+    @Query("select count(fr.receiver.receivedRequests) from FriendRequest fr, Customer c where fr.receiver.uId = c.uId and fr.receiver.uId = :receiverId")
+    List<FriendRequest> findNumOfReceivedRequests(@Param("receiverId") Long receiverId);
+
+    @Query("select count(fr.sender.sentRequests) from FriendRequest fr, Customer c where fr.sender.uId = c.uId and fr.sender.uId = :senderId")
+    List<FriendRequest> findNumOfSendedRequests(@Param("senderId") Long senderId);
 
 }
