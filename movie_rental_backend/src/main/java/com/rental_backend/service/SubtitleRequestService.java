5665b37ac513696f783c8f2b7dedd92534e75cce
YagmurEryilmaz <yagmur.eryilmaz@ug.bilkent.edu.tr>
2022-04-28 16:03:27 +0300

    Service classes are updated

--- movie_rental_backend/src/main/java/com/rental_backend/service/SubtitleRequestService.java
+++ movie_rental_backend/src/main/java/com/rental_backend/service/SubtitleRequestService.java
@@ -1,4 +1,11 @@
 package com.rental_backend.service;
+import com.rental_backend.entity.*;
+import com.rental_backend.repository.*;
+import org.hibernate.annotations.SQLInsert;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
 public class SubtitleRequestService {
 }
