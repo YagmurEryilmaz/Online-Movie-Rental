284cffbe53e78b7150026c07d6c20abd6c5fbecb
cekole <63546159+cekole@users.noreply.github.com>
2022-04-30 14:12:11 +0300

    MovieService update

--- movie_rental_backend/src/main/java/com/rental_backend/service/MovieService.java
+++ movie_rental_backend/src/main/java/com/rental_backend/service/MovieService.java
@@ -6,8 +6,34 @@ import org.hibernate.annotations.SQLInsert;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
     private MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    public List<Movie> findMovieByTitle(String title) {
+        return movieRepository.findByTitle(title);
+    }
+
+    public List<Movie> findMovieByGenre(String genre) {
+        return movieRepository.findByGenre(genre);
+    }
+
+    public List<Movie> findMovieByProdYear(int prodYear) {
+        return movieRepository.findByProductionYear(prodYear);
+    }
+
+    public MovieRepository getMovieRepository() {
+        return movieRepository;
+    }
+
+    public void setMovieRepository(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
 }
