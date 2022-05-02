0f76e0d37a0fab7d3a166aab402ef4567103eb63
YagmurEryilmaz <yagmur.eryilmaz@ug.bilkent.edu.tr>
2022-05-02 16:54:59 +0300

    movieController is added

--- movie_rental_backend/src/main/java/com/rental_backend/controller/MovieController.java
+++ movie_rental_backend/src/main/java/com/rental_backend/controller/MovieController.java
@@ -1,20 +1,27 @@
 package com.rental_backend.controller;
 
+import com.rental_backend.dto.MovieResponse;
 import com.rental_backend.entity.Movie;
 import com.rental_backend.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
+@RestController
 public class MovieController {
+
     private MovieService movieService;
     @Autowired
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
-    @GetMapping
+    @GetMapping("/getAllMovies")
     public ResponseEntity<List<Movie>> getAllMovies() {
         return ResponseEntity.ok(movieService.getAllMovies());
     }
