78d6a62c80a06ceec6f04b95a6a8c893ee21da1a
cekole <63546159+cekole@users.noreply.github.com>
2022-05-04 17:56:18 +0300

    minor update

--- movie_rental_backend/src/main/java/com/rental_backend/RentalBackendApplication.java
+++ movie_rental_backend/src/main/java/com/rental_backend/RentalBackendApplication.java
@@ -29,17 +29,17 @@ public class RentalBackendApplication {
     @Bean
     InitializingBean databaseInit() {
         return () -> {
-            movieRepository.save(new Movie(21001,"Into the Wild" ,"Adventure","Can Önal",2002, 49.9, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2022,05,31)));
-            movieRepository.save(new Movie(21002,"Up" ,"Animation","Yağmur Eryılmaz",2006, 35, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2021,05,31)));
-            movieRepository.save(new Movie(21003,"Pride and Prejudice" ,"Romantic","Elif Cenesiz",1999, 65.5, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2020,05,31)));
-            movieRepository.save(new Movie(21004,"Iron Man" ,"Action","Can Önal",2005, 50.5, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2020,05,10)));
-            movieRepository.save(new Movie(21005,"Darkness In the Shadow" ,"Action","Cenk Duran",2009, 90, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2019,05,15)));
-            movieRepository.save(new Movie(21006,"Undefined Memories" ,"Romantic","Can Önal",1997, 60.5, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2020,07,29)));
+            movieRepository.save(new Movie(21001,"Into the Wild" ,"Adventure","Can Önal",102, 49.9, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2022,05,31)));
+            movieRepository.save(new Movie(21002,"Up" ,"Animation","Yağmur Eryılmaz",106, 35, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2021,05,31)));
+            movieRepository.save(new Movie(21003,"Pride and Prejudice" ,"Romantic","Elif Cenesiz",99, 65.5, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2020,05,31)));
+            movieRepository.save(new Movie(21004,"Iron Man" ,"Action","Can Önal",105, 50.5, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2020,05,10)));
+            movieRepository.save(new Movie(21005,"Darkness In the Shadow" ,"Action","Cenk Duran",109, 90, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2019,05,15)));
+            movieRepository.save(new Movie(21006,"Undefined Memories" ,"Romantic","Can Önal",97, 60.5, "https://image.tmdb.org/t/p/w440_and_h660_face/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", new Date(2020,07,29)));
 
-            userAccountRepository.save(new UserAccount(0000000001, "Can Önal" ,"can", new Date(2002,02,05),"bjk@gmail.com","customer"));
-            userAccountRepository.save(new UserAccount(0000000002, "Yağmur Eryılmaz" ,"yağmur", new Date(2006,01,01),"yagmurery12@gmail.com","customer"));
-            userAccountRepository.save(new UserAccount(0000000003, "Elif Cenesiz" ,"elif", new Date(2010,04,07),"elif@gmail.com","customer"));
-            userAccountRepository.save(new UserAccount(0000000004, "Cenk Duran" ,"cenk", new Date(1982,07,07),"cekoley@gmail.com","admin"));
+            userAccountRepository.save(new UserAccount(0000000001, "Can Önal" ,"can", new Date(102,02,05),"bjk@gmail.com","customer"));
+            userAccountRepository.save(new UserAccount(0000000002, "Yağmur Eryılmaz" ,"yağmur", new Date(106,01,01),"yagmurery12@gmail.com","customer"));
+            userAccountRepository.save(new UserAccount(0000000003, "Elif Cenesiz" ,"elif", new Date(110,04,07),"elif@gmail.com","customer"));
+            userAccountRepository.save(new UserAccount(0000000004, "Cenk Duran" ,"cenk", new Date(82,07,07),"cekoley@gmail.com","admin"));
         };
     }
 
