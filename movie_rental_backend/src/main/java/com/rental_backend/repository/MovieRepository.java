ceb917d807d8c7ba5c333db9d432b816dd855b49
cekole <63546159+cekole@users.noreply.github.com>
2022-05-05 14:48:22 +0300

    minor update on delete

--- movie_rental_backend/src/main/java/com/rental_backend/repository/MovieRepository.java
+++ movie_rental_backend/src/main/java/com/rental_backend/repository/MovieRepository.java
@@ -3,10 +3,12 @@ package com.rental_backend.repository;
 
 import com.rental_backend.entity.Movie;
 import com.rental_backend.entity.UserAccount;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -27,6 +29,8 @@ public interface MovieRepository extends CrudRepository<Movie,Long>{
             "or lower(m.directorName) like lower(concat('%', :searchTerm, '%'))")
     List<Movie> search(@Param("searchTerm") String searchTerm);
 
+    @Transactional
+    @Modifying
     @Query("delete from Movie where title= :title and directorName = :directorName")
     List<Movie> deleteMovie(@Param("title") String title, @Param("directorName") String directorName);
 
